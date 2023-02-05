690ba186197bccacbf324b7de4359cdd88952ec4
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-06 00:13:11 +0600

    footer motion

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -1,12 +1,31 @@
 'use client';
 
 import React from 'react';
+import { motion } from 'framer-motion';
+import { fromBottomVariants } from '@/utils/motion';
+import styles from '@/styles';
 
 const Footer = () => {
+  const year = new Date()?.getFullYear();
   return (
-    <div>
-      <h1>Hello there</h1>
-    </div>
+    <motion.div
+      variants={fromBottomVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.xPaddings} pt-6 mt-auto relative`}
+    >
+      <div className="footer footer-center p-4">
+        <div>
+          <p>
+            Copyright &copy; {year} - All right reserved by{' '}
+            <span className="text-xs text-primary font-extrabold sm:text-sm md:text-xl">
+              {' '}
+              Agency{' '}
+            </span>
+          </p>
+        </div>
+      </div>
+    </motion.div>
   );
 };
 
