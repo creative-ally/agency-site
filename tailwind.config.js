fc91bc23aae15e78abd4a130381319fbdffbb12c
Neamul01 <neamulhaque2002@gmail.com>
2023-02-10 21:07:16 +0600

    projects section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     colors: {
-      "primary": "#015249",
-      "secondary": "#57BC90",
-      "accent": "#77C9D4",
-      "neutral": "#A5A5AF",
+      primary: "#015249",
+      secondary: "#57BC90",
+      accent: "#77C9D4",
+      neutral: "#A5A5AF",
       "base-100": "#FFFFFF",
-      "dark": {
-        "primary": "#F64668",
-        "secondary": "#984063",
-        "accent": "#41436A",
-        "neutral": "#2A3950",
+      dark: {
+        primary: "#F64668",
+        secondary: "#984063",
+        accent: "#41436A",
+        neutral: "#2A3950",
         "base-100": "#FFFFFF",
-      }
+      },
     },
     extend: {},
   },
