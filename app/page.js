fd6c0475e3b7a68fa71635448988f226e74d402c
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-10 21:59:47 +0600

    issue fixed on scrolling

--- app/page.js
+++ app/page.js
@@ -8,13 +8,9 @@ import Blogs from "@/components/Blogs/blogs"
 export default function Home() {
   return (
     <div className="my-4">
-<<<<<<< HEAD
       <Banner />
       <Projects />
-=======
-      <Banner/>
       <Blogs/>
->>>>>>> 4a4dea71f0de35e7127f6f29951245187af6c971
     </div>
   );
 }
