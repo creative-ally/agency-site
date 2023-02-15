6c7d8c229fbb00974bc9a7ea47f1da682183bd92
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-15 19:09:39 +0600

    member added

--- components/TeamMembers/Member.jsx
+++ components/TeamMembers/Member.jsx
@@ -6,29 +6,29 @@ import { FaFacebookF, FaLinkedin, FaLinkedinIn, FaTwitter } from 'react-icons/fa
 import memberImage from '../../assets/images/authorimg.jpg'
 import memberBg from '../../assets/images/memberbg.png'
 
-const Member = ({member}) => {
+const Member = ({ member }) => {
     return (
         <div className='group relative'>
-            <Card sx={{ maxWidth: 345 }}>
+            <Card sx={{ height: 370 }}>
                 {/* <Image src={member.image} width={400} height={400} alt="" /> */}
                 <Image className='w-full h-full' src={memberImage} alt="" />
-                <div className='h-32 w-full absolute mt-[-128px] invisible group-hover:visible group-hover:transition-all group-hover:easy-in group-hover:duration-1000' style={{
+                <div className='h-32 w-full absolute mt-[-128px] lg:invisible lg:group-hover:visible group-hover:transition-all group-hover:easy-in group-hover:duration-1000' style={{
                     backgroundImage: `url(${memberBg.src})`,
-                    // width: '100%',
-
                     backgroundPosition: 'center',
                     backgroundSize: 'cover'
                 }}>
                     <ul className='flex justify-center items-center h-full '>
-                        <li className='invisible group-hover:visible text-base-100 hover:text-primary dark:hover:text-dark-primary mx-1 group-hover:border rounded-full p-2 hover:bg-base-100 '><a href="#"><FaFacebookF className='' size={25} /></a></li>
-                        <li className='invisible group-hover:visible text-base-100 hover:text-primary dark:hover:text-dark-primary mx-1 group-hover:border rounded-full p-2 hover:bg-base-100 '><a href="#"> <FaTwitter className='' size={25} /></a></li>
-                        <li className='invisible group-hover:visible text-base-100 hover:text-primary dark:hover:text-dark-primary mx-1 group-hover:border rounded-full p-2 hover:bg-base-100 '><a href="#"> <FaLinkedinIn className='' size={25} /> </a></li>
-                        <li className='invisible group-hover:visible text-base-100 hover:text-primary dark:hover:text-dark-primary mx-1 group-hover:border rounded-full p-2 hover:bg-base-100 '><a href="#"> <FiGithub className='invisible group-hover:visible' size={25} /> </a></li>
+                        <li className='cursor-pointer text-base-100 hover:text-primary dark:hover:text-dark-primary mx-1 border rounded-full p-2 hover:bg-base-100 '><a href={member.social[0]}><FaFacebookF className='' size={25} /></a></li>
+                        <li className='cursor-pointer text-base-100 hover:text-primary dark:hover:text-dark-primary mx-1 border rounded-full p-2 hover:bg-base-100 '><a href={member.social[1]}> <FaTwitter className='' size={25} /></a></li>
+                        <li className='cursor-pointer text-base-100 hover:text-primary dark:hover:text-dark-primary mx-1 border rounded-full p-2 hover:bg-base-100 '><a href={member.social[2]}> <FaLinkedinIn className='' size={25} /> </a></li>
+                        <li className='cursor-pointer text-base-100 hover:text-primary dark:hover:text-dark-primary mx-1 border rounded-full p-2 hover:bg-base-100 '><a href={member.social[3]}> <FiGithub className='' size={25} /> </a></li>
                     </ul>
                 </div>
             </Card>
-            <h3 className='text-center text-lg md:text-xl lg:text-2xl uppercase font-semibold text-black dark:text-base-100 pt-4 group-hover:text-primary dark:group-hover:text-dark-primary'>{member.name}</h3>
-            <p className='text-center lg:lg uppercase font-semibold text-black dark:text-base-100 pt-1'>{member.designation}</p>
+            <div className='text-center mb-5 lg:mb-0'>
+                <h3 className='text-lg md:text-xl lg:text-2xl uppercase font-semibold text-black dark:text-base-100 pt-4 group-hover:text-primary dark:group-hover:text-dark-primary'>{member.name}</h3>
+                <p className='lg:lg uppercase font-semibold text-black dark:text-base-100 pt-1'>{member.designation}</p>
+            </div>
         </div>
     );
 };
--- components/TeamMembers/Members.jsx
+++ components/TeamMembers/Members.jsx
@@ -9,7 +9,11 @@ import Member from './Member';
 
 const Members = () => {
     return (
-        <div>
+        <div className=''>
+            <div className='text-center pb-5 md:pb-7 lg:pb-10 '>
+                <p className="text-lg md:text-xl lg:text-2xl uppercase font-semibold text-primary dark:text-dark-primary">Our skilled team</p>
+                <h2 className="text-2xl md:text-3xl lg:text-5xl uppercase font-semibold text-black dark:text-base-100 pt-3 lg:pt-5">Meet the <span className='text-primary dark:text-dark-primary'>leaders</span></h2>
+            </div>
             <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-5'>
                 {
                     members.map((member, i) =>
@@ -33,9 +37,9 @@ const members = [
         "designation": "Software Engineer",
         "social": [
             "https://www.facebook.com/",
-            "https://www.linkedin.com/feed/",
-            "https://github.com/",
-            "https://stackoverflow.com/"
+            "https://twitter.com/",
+            "https://www.linkedin.com/",
+            "https://github.com/"
         ]
     },
     {
@@ -45,9 +49,9 @@ const members = [
         "designation": "Software Engineer",
         "social": [
             "https://www.facebook.com/",
-            "https://www.linkedin.com/feed/",
-            "https://github.com/",
-            "https://stackoverflow.com/"
+            "https://twitter.com/",
+            "https://www.linkedin.com/",
+            "https://github.com/"
         ]
     },
     {
@@ -57,9 +61,9 @@ const members = [
         "designation": "Software Engineer",
         "social": [
             "https://www.facebook.com/",
-            "https://www.linkedin.com/feed/",
-            "https://github.com/",
-            "https://stackoverflow.com/"
+            "https://twitter.com/",
+            "https://www.linkedin.com/",
+            "https://github.com/"
         ]
     },
     {
@@ -69,9 +73,9 @@ const members = [
         "designation": "Software Engineer",
         "social": [
             "https://www.facebook.com/",
-            "https://www.linkedin.com/feed/",
-            "https://github.com/",
-            "https://stackoverflow.com/"
+            "https://twitter.com/",
+            "https://www.linkedin.com/",
+            "https://github.com/"
         ]
     },
 ]
