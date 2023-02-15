4808d141ac7266a43f8064373a659b971e4e4b3d
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-02-15 19:13:38 +0600

    issue fixed

--- app/about/page.js
+++ app/about/page.js
@@ -7,8 +7,11 @@ import aboutImage from '../../assets/images/6.png'
 export default function Page() {
   return (
     <div className="p-5 md:p-10 lg:p-32 ">
-      <div className="flex justify-between items-center ">
-        <div className="w-7/12 pr-5 lg:pr-10 mb-5">
+      <div className="md:flex flex-row-reverse justify-between items-center ">
+      <div className="md:w-5/12 pl-5 lg:pl-10">
+          <Image className="w-full h-full" src={aboutImage} alt='' />
+        </div>
+        <div className="md:w-7/12 pr-5 lg:pr-10 mb-5">
           <p className="text-lg md:text-xl lg:text-2xl uppercase font-semibold text-primary dark:text-dark-primary">Why Choose Us</p>
           <h3 className="text-2xl md:text-3xl lg:text-5xl uppercase font-semibold text-black dark:text-base-100 pt-3 lg:pt-5">GREAT Solutions For</h3>
           <h3 className="text-2xl md:text-3xl lg:text-5xl uppercase font-semibold text-black dark:text-base-100 pt-3 lg:pt-5">Your <span className="text-2xl md:text-3xl lg:text-5xl uppercase font-semibold text-primary dark:text-dark-primary pt-2 lg:pt-5"> Business.</span></h3>
@@ -21,12 +24,8 @@ export default function Page() {
           </div>
           <Globalbtn>Read More</Globalbtn>
         </div>
-       
-        <div className="w-5/12 pl-5 lg:pl-10">
-          <Image className="w-full h-full" src={aboutImage} alt='' />
-        </div>
       </div>
-      <div className="h-32"></div>  
+      <div className="h-5 md:h-16 lg:h-32"></div>  
       <div>
         <Members />
       </div>
