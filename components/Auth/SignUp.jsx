07d676c0e59bbf2ccb9af37c424f94ec1db8c956
Mahbub Noyon <mahbubnoyon506@gmail.com>
2023-03-03 00:29:59 +0600

    user credential added

--- components/Auth/SignUp.jsx
+++ components/Auth/SignUp.jsx
@@ -9,34 +9,51 @@ import {
   TextField,
 } from '@mui/material';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
 
 const Regi = () => {
+
+  const router = useRouter()
+
+  const { register, formState: { errors }, handleSubmit, reset } = useForm();
+  const onSubmit = async (data) => {
+    const res = await fetch('/api/auth/signup', {
+         method: 'POST',
+         headers: {
+             'content-type' : 'application/json'
+         },
+         body: JSON.stringify(data),
+         callbackUrl: '/'
+     })
+     console.log(res)
+     if(res.status === 200){
+         alert('Data added successfully')
+         router.push(res.url)
+         reset()
+     }
+ };
+
   return (
     <div>
-      <Box component="form" noValidate sx={{ mt: 1 }} id="registration-form">
+      <Box onSubmit={handleSubmit(onSubmit)} component="form" noValidate sx={{ mt: 1 }} id="registration-form">
         <TextField
           margin="normal"
           required
           fullWidth
-          id="name"
-          name="name"
-          label="Name"
+          {...register("name", { required: "Email Address is required" })}
         />
         <TextField
           margin="normal"
           required
           fullWidth
-          id="email"
-          name="email"
-          label="Email Address"
+          {...register("email", { required: "Email Address is required" })}
         />
         <TextField
           margin="normal"
           required
           fullWidth
-          id="password"
-          name="password"
-          label="Password"
+          {...register("password", { required: "Email Address is required" })}
           type="password"
         />
         <TextField
