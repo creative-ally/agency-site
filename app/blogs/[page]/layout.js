08fdfbbe618c227ad76673888839f1c1cac9065d
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2023-03-11 22:26:05 +0600

    blog and single blog

---
+++ app/blogs/[page]/layout.js
@@ -0,0 +1,14 @@
+import SingleBlog from '@/components/Blogs/SingleBlog';
+import React from 'react';
+
+const singleblog = () => {
+    return (
+        <div>
+            <SingleBlog/>
+        </div>
+
+        
+    );
+};
+
+export default singleblog;
